0a5ac246bb130931fca2c2c93dff99ec63b102fd
MarcoRomanoHub <marcoromano270@gmail.com>
2024-04-18 16:23:23 +0200

    [add] POST add newDisk

--- script.js
+++ script.js
@@ -5,12 +5,28 @@ createApp({
   data() {
     return {
       apiUrl: 'server.php',
-      list: []
+      list: [],
+      newDisk: {
+        title: '',
+        author: '',
+        poster: '',
+      }
     }
   },
   methods: {
     getApi() {
       axios.get(this.apiUrl)
+        .then(result => {
+          this.list = result.data;
+        })
+    },
+    addNewDisk() {
+      const data = new FormData();
+      data.append('newDiskTitle', this.newDisk.title);
+      data.append('newDiskAuthor', this.newDisk.author);
+      data.append('newDiskPoster', this.newDisk.poster);
+
+      axios.post(this.apiUrl, data)
         .then(result => {
           this.list = result.data;
         })
--- server.php
+++ server.php
@@ -4,6 +4,18 @@ $json_string = file_get_contents('dischi.json');
 $list = json_decode($json_string);
 
 
+if (isset($_POST['newDiskTitle'])) {
+  $new_item = [
+    'title' => $_POST['newDiskTitle'],
+    'author' => $_POST['newDiskAuthor'],
+    'poster' => $_POST['newDiskPoster'],
+
+  ];
+  $list[] = $new_item;
+  file_put_contents('dischi.json', json_encode($list));
+}
+
+
 header('Content-Type: application/json');
 
 echo json_encode($list);
